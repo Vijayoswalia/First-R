c0c2805293497dfb86bfe6fa15cf3d6f20b4f0f3
Vijayoswalia <vijayoswalia@gmail.com>
2018-04-11 10:53:49 +0530

    Vidooly

--- oct.R
+++ oct.R
@@ -1,29 +1,44 @@
-library(vars)
+library(vars) # ur.df,
 library(forecast)
 library(urca)
+library(xts)
+library(ggplot2)
+library(dplyr)
 oct_march = read.csv("oct_march.csv", stringsAsFactors = F)
+test = oct_march
+test = oct_march[-5]
+plot(ts1)
+test[,1]
 str(oct_march)
+
+
+
+head(oct_march)
 View(oct_march)
-adf.test(oct_march, alternative = "stationary")
+oct_march$date = as.Date(oct_march$date, format = "%y-%m-%d")
+planes <- group_by(test, test$chid)
+planes
+
+a = group_by(oct_march,chid)
+  
+#adf.test(oct_march, alternative = "stationary")
 w = ur.df(oct_march$videoscount,type = "drift", selectlags = "AIC")
 summary(w) #w is stationary
 x = ur.df(oct_march$views,type = "drift", selectlags = "AIC")
 summary(x) #x is stationary
 y = ur.df(oct_march$subscriber,type = "drift", selectlags = "AIC")
 summary(y) #y is stationary
-oct = cbind(oct_march$chid, oct_march$views,oct_march$subscriber,oct_march$videoscount)
-oct_time = cbind(oct, oct_march$date)
-oct_march_TS = ts(oct_time, start = c(2016,10), frequency = 12)
+
+o_var=VAR(oct_march[,-1], type = "const", lag.max = 10, ic="AIC")
 
 
-oct1 = cbind(oct_march$views,oct_march$subscriber,oct_march$videoscount)
-oct_time1 = cbind(oct1, oct_march$date)
-oct_march_TS1 = ts(oct_time1, start = c(2016,10), end = c(2017,03), frequency =12)
-class(oct_march_TS1)
-?ts
-oct_march_TS1
-TS = oct_march_TS1[,-5]
-colnames(TS) = c( "chid","views","subscriber","videoscount")
-TS = ts(TS)
-----------------------------------------------
-class(TS)
+
+
+
+
+
+
+
+
+
+
